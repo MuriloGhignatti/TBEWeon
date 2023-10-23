0cb19dbe31e6ab8183cadaefacd61ec8a2477049
Murilo Ghignatti <muriloghignatti@gmail.com>
2023-10-23 17:11:49 -0300

    Adding a reflection hack so we can isolate tests using the DAO Singleton class.

--- src/test/java/me/murilo/ghignatti/ProducerTest.java
+++ src/test/java/me/murilo/ghignatti/ProducerTest.java
@@ -12,11 +12,12 @@ import java.util.concurrent.FutureTask;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ConsumerTest {
+class ProducerTest {
 
     private OpenChannelDAO dao;
 
-    void setupDao(){
+    @BeforeEach
+    void setup(){
         Constructor<OpenChannelDAOImpl> cons =
                 (Constructor<OpenChannelDAOImpl>) OpenChannelDAOImpl.class.getDeclaredConstructors()[0];
         cons.setAccessible(true);
@@ -26,10 +27,8 @@ class ConsumerTest {
             fail();
         }
     }
-
-    @BeforeEach
-    void setup(){
-        setupDao();
+    @Test
+    void call() {
         Producer producer = new Producer(ChatChannelFactory.getInstance(), dao, 1);
         FutureTask<Void> futureTask = new FutureTask<>(producer);
         Thread thread = new Thread(futureTask);
@@ -40,23 +39,10 @@ class ConsumerTest {
                 Thread.sleep(2);
             }
             thread.interrupt();
-        } catch (Exception e) {
-            fail();
-        }
-    }
-
-    @Test
-    void call() {
-        Consumer consumer = new Consumer(dao, 1);
-        FutureTask<Void> futureTask = new FutureTask<>(consumer);
-        Thread thread = new Thread(futureTask);
-        thread.start();
-        try {
-            futureTask.get();
             //Even with 1 ms delay and waiting for only 2 ms we can still have more than 1 channel produced,
             //  we could make an implementation limiting the amount of channels produced by thread, but I think this
             //  goes against the idea.
-            assertTrue(dao.getConsumed() >= 1);
+            assertTrue(dao.getProduced() >= 1);
         } catch (Exception e) {
             fail();
         }
--- src/test/java/me/murilo/ghignatti/data/OpenChannelDAOImplTest.java
+++ src/test/java/me/murilo/ghignatti/data/OpenChannelDAOImplTest.java
@@ -1,22 +1,31 @@
 package me.murilo.ghignatti.data;
 
 import me.murilo.ghignatti.channelfactories.ChatChannelFactory;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+import static org.junit.jupiter.api.Assertions.*;
+
 class OpenChannelDAOImplTest {
 
-    private final OpenChannelDAO dao = OpenChannelDAOImpl.getInstance();
+    private OpenChannelDAO dao;
+
+    @BeforeEach
+    void setup(){
+        Constructor<OpenChannelDAOImpl> cons =
+                (Constructor<OpenChannelDAOImpl>) OpenChannelDAOImpl.class.getDeclaredConstructors()[0];
+        cons.setAccessible(true);
+        try {
+            dao = cons.newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            fail();
+        }
+    }
 
     @Test
-    @Order(1)
     void addOpenChannel() {
         assertTrue(dao.isQueueEmpty());
         dao.addOpenChannel(ChatChannelFactory.getInstance().produceServiceChannel());
@@ -24,31 +33,29 @@ class OpenChannelDAOImplTest {
     }
 
     @Test
-    @Order(2)
     void getOpenChannel() {
-        assertFalse(dao.isQueueEmpty());
+        assertTrue(dao.isQueueEmpty());
+        dao.addOpenChannel(ChatChannelFactory.getInstance().produceServiceChannel());
         dao.getOpenChannel(1);
         assertTrue(dao.isQueueEmpty());
     }
 
     @Test
-    @Order(3)
     void getProduced() {
-        assertEquals(1, dao.getProduced());
-        assertEquals(1, dao.getConsumed());
+        assertEquals(0, dao.getProduced());
+        assertEquals(0, dao.getConsumed());
         dao.addOpenChannel(ChatChannelFactory.getInstance().produceServiceChannel());
-        assertEquals(2, dao.getProduced());
-        assertEquals(1, dao.getConsumed());
+        assertEquals(1, dao.getProduced());
+        assertEquals(0, dao.getConsumed());
     }
 
     @Test
-    @Order(4)
     void getConsumed() {
-        assertEquals(2, dao.getProduced());
-        assertEquals(1, dao.getConsumed());
+        assertEquals(0, dao.getProduced());
+        assertEquals(0, dao.getConsumed());
         dao.addOpenChannel(ChatChannelFactory.getInstance().produceServiceChannel());
         dao.getOpenChannel(1);
-        assertEquals(3, dao.getProduced());
-        assertEquals(2, dao.getConsumed());
+        assertEquals(1, dao.getProduced());
+        assertEquals(1, dao.getConsumed());
     }
 }
